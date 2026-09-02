3f6f21e3da914d40b480266adc9fe8c9ffdc9e0a
HR DIGITAL TV <arifhoussin1993@gmail.com>
2026-09-02 18:14:14 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+// ==========================================
+// 📐 রুট প্রজেক্ট লেভেল প্লাগইন কনফিগারেশন
+// ==========================================
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
